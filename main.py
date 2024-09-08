0c3b385ecc1f632529b1c994664b205be5dca326
3sub <flipper310606@gmail.com>
2024-09-08 22:02:27 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+
+# YOUR TOKEN DISCORD
+token = ""
+
+# Message à envoyer aux amis
+message = '''
+discord.gg/black-gold
+'''
+
+client = discord.Client()
+
+# Event
+@client.event
+async def on_connect():
+    user_messaged = 0  # User counter
+
+    # friend loop
+    for user in client.user.friends:
+        try:
+            # Send message to every friend
+            await user.send(message)
+            print(f"Message envoyé à : {user.name}")
+            user_messaged += 1 
+        except Exception as e:
+            print(f"Erreur lors de l'envoi à {user.name}: {str(e)}")
+            pass
+    
+    # Displaying the total number of users
+    print(f"\n\n\n{user_messaged} utilisateurs ont reçu le message !")
+
+client.run(token, bot=False)
